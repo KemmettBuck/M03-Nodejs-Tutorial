ac01bcf56aa3e4196fb4c1590a214cdb45bcb01a
Kira Buck <kemmettbuck@ivytech.edu>
2025-04-03 10:49:02 -0400

    send html to browser via server js and index html files

--- M03-Nodejs-Tutorial/server.js
+++ M03-Nodejs-Tutorial/server.js
@@ -1,15 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
-
-    res.write('<head><link rel="stylesheet" href="#"></head>')
-    res.write('<p>hello, ninjaaas</p>');
-    res.write('<p>hello again, ninjaaas</p>');
-    res.end();
+// send html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
 }); 
 
 server.listen(3000, 'localhost', () => {
