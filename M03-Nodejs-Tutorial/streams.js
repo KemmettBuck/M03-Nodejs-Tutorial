d5b831afe5dce5902bdd5450193f380b27846b71
Kira Buck <kemmettbuck@ivytech.edu>
2025-04-03 09:17:32 -0400

    delete and rewrite blog4 with stream

--- M03-Nodejs-Tutorial/streams.js
+++ M03-Nodejs-Tutorial/streams.js
@@ -5,9 +5,12 @@ const readStream = fs.createReadStream('./docs/blog3.txt', {
 });
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-readStream.on('data', (chunk) => {
-    console.log('---- NEW CHUNK ----');
-    console.log(chunk);
-    writeStream.write('\nNEW CHUNK\n');
-    writeStream.write(chunk);
-});
+// readStream.on('data', (chunk) => {
+//     console.log('---- NEW CHUNK ----');
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// });
+
+// piping
+readStream.pipe(writeStream);
