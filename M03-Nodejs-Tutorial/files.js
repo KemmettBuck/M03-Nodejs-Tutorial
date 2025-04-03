88dd2571097a4902e4d78c8af9e31b9dabce6943
Kira Buck <kemmettbuck@ivytech.edu>
2025-04-02 22:45:26 -0400

    write files blog2 hello again

--- M03-Nodejs-Tutorial/files.js
+++ M03-Nodejs-Tutorial/files.js
@@ -15,4 +15,8 @@ fs.writeFile('./docs/blog1.txt', 'hello, world', () => {
     console.log('file was written');
 });
 
+fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+    console.log('file was written');
+});
+
 // deleting files
