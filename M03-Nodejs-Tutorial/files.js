7bcd75bda18d0b1ec204eb363c4bed28ec81aedd
Kira Buck <kemmettbuck@ivytech.edu>
2025-04-02 23:06:21 -0400

    delete txt file deleteme

--- M03-Nodejs-Tutorial/files.js
+++ M03-Nodejs-Tutorial/files.js
@@ -38,3 +38,11 @@ if (!fs.existsSync('./assets')) {
 }
 
 // deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('file deleted');
+    })
+}
