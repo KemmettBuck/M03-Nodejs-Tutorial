d822fe6fe974c0fe68548f9d99064732dadf2ca2
Kira Buck <kemmettbuck@ivytech.edu>
2025-04-04 08:56:58 -0400

    make create ejs file

--- M03-Nodejs-Tutorial/app.js
+++ M03-Nodejs-Tutorial/app.js
@@ -18,16 +18,14 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
 
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
-
+    res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render();
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
